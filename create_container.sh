ce3c1964a2c2f2cd9077134f6519a1301267fa26
Curtis Bunch <me@cudabu.io>
2024-04-02 09:52:52 -0500

    changed container setup

--- create_container.sh
+++ create_container.sh
@@ -5,11 +5,7 @@ set -e
 container_name="att-ipv6"
 container_root_password=""
 
-vlan_id=7
-vlan_address="192.168.7.3/24"
-vlan_gateway="192.168.7.2"
-
-function create_custom_container() {
+function create_container() {
 
 if [ -d "/data/custom/machines/$container_name" ] 
 then
@@ -30,53 +26,19 @@ mkdir -p /var/lib/machines
 ln -s /data/custom/machines/"$container_name" /var/lib/machines/
 }
 
-function setup_networking_macvlan() {
+function setup_networking() {
 mkdir -p /etc/systemd/nspawn
 
 cat <<EOF > /etc/systemd/nspawn/"$container_name".nspawn
 [Exec]
 Boot=on
+Capability=all
 
 [Network]
-MACVLAN=br$vlan_id
+Private=off
+VirtualEthernet=off
 ResolvConf=off
 EOF
-
-##### Configure the Container to use an Isolated MacVLAN Network
-cd /data/on_boot.d
-if [ -f "$file" ] ; then
-    rm "$file"
-fi
-curl -LO https://raw.githubusercontent.com/cudabu/udm-ipv6/main/10-setup-network.sh
-chmod +x 10-setup-network.sh
-
-cat <<EOF > /etc/systemd/nspawn/"$container_name".nspawn
-[Exec]
-Boot=on
-
-[Network]
-MACVLAN=br$vlan_id
-ResolvConf=off
-EOF
-
-#####Configure your container to set the IP and gateway you defined in 10-setup-network.sh
-cd /data/custom/machines/"$container_name"/etc/systemd/network
-
-cat <<EOF > mv-br${vlan_id}.network
-[Match]
-Name=mv-br$vlan_id
-
-[Network]
-IPForward=yes
-Address=$vlan_address
-Gateway=$vlan_gateway
-EOF
-
-#### Run the 10-setup-network.sh script to setup the network interface
-/data/on_boot.d/10-setup-network.sh
-machinectl stop "$container_name"
-machinectl start "$container_name"
-
 }
 
 function setup_persistence() {
@@ -87,7 +49,6 @@ chmod +x 0-setup-system.sh
 mv 0-setup-system.sh 02-setup-system.sh
 }
 
-# Call the menu function
-create_custom_container
-setup_networking_macvlan
+create_container
+setup_networking
 setup_persistence
